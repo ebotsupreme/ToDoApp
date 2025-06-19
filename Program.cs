1051f8e78bf1fed65fbb2244f36ab6e5d46ea013
ebotsupreme <ejung716@gmail.com>
2025-06-19 08:35:57 -0700

    Add ViewTasks feature. Users are now able to view tasks or exit. Incorrect input prompts invalid msg.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<string> tasks = new List<string>();
         bool running = true;
 
         while (running)
         {
+            Console.WriteLine();
             Console.WriteLine("What woud you like to do?");
             Console.WriteLine("1. View tasks");
             Console.WriteLine("2. Add a task");
@@ -17,9 +19,50 @@ class Program
             Console.Write("> ");
 
             string input = Console.ReadLine();
+            HandleMenuSelection(input, tasks, ref running);
+        }
 
-            // For now exit after one run
-            running = false;
+        static void HandleMenuSelection(string input, List<string> tasks, ref bool running)
+        {
+            switch (input)
+            {
+                case "1": // view tasks
+                    ViewTasks(tasks);
+                    break;
+                // case "2": // add tasks
+                //     break;
+                // case "3": // Mark task as done
+                //     break;
+                // case "4": // Delete a task
+                //     break;
+                case "5": // Exit
+                    Console.WriteLine();
+                    Console.Write("Exiting To-Do App...");
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+        
+        static void ViewTasks(List<string> tasks)
+        {
+            if (tasks.Count > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Your tasks:");
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {tasks[i]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("No tasks found.");
+            }
         }
     }
 }
